6f2bf305c4c803210b7da7cd92f84a7589d556a0
indeep99 <indeep.singh99@gmail.com>
2022-01-31 13:19:51 -0500

    test

--- dallas-map/src/service/dallasService.js
+++ dallas-map/src/service/dallasService.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 class DallasService {
   findHouses(lng, lat, radius) {
-    return axios.get(`http://localhost:8080/dallas/${lng}/${lat}/${radius}`);
+    return axios.get(`https://dallas-backend.herokuapp.com/dallas/${lng}/${lat}/${radius}`);
   }
 }
 
