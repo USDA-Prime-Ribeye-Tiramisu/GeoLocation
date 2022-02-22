4dd9dc03d9abc75c7ba8cd505c4a278eab8b1bf0
indeep99 <indeep.singh99@gmail.com>
2022-02-22 17:11:30 -0500

    testing get mapper

--- geolocation/src/main/java/com/dallas/rent/geolocation/controller/DallasController.java
+++ geolocation/src/main/java/com/dallas/rent/geolocation/controller/DallasController.java
@@ -31,7 +31,7 @@ public class DallasController {
     }
 
     @CrossOrigin
-    @GetMapping()
+    @GetMapping(value = "/hello")
     public String helloWorld() {
         return "Hello World";
     }
