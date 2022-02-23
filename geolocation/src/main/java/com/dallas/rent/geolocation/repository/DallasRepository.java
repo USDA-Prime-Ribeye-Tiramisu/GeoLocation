03fd05c6d9d6a84cfaca78754821e43eb55480c1
indeep99 <indeep.singh99@gmail.com>
2022-02-22 19:15:48 -0500

    in string

--- geolocation/src/main/java/com/dallas/rent/geolocation/repository/DallasRepository.java
+++ geolocation/src/main/java/com/dallas/rent/geolocation/repository/DallasRepository.java
@@ -15,7 +15,12 @@ public interface DallasRepository extends JpaRepository<Dallas, Long> {
     @Query(value = "SELECT * from dallas_rent where ST_DistanceSphere(geom, ST_MakePoint(:lon, :lat)) <= :distanceM", nativeQuery = true)
     List<Dallas> findNearWithinDistance(@Param("lon") Double lon, @Param("lat") Double lat,
             @Param("distanceM") double distanceM);
-
-    @Query(value="SELECT * from dallas_rent where ST_DistanceSphere(geom, ST_MakePoint(:lon, :lat)) <= :distance ORDER BY ST_Distance (geom\\:\\:geography, ST_Point(:lon, :lat)\\:\\:geography) ASC LIMIT :max", nativeQuery = true)
+    
+     
+    String query = "SELECT mls,rent from dallas_rent" +
+                "where ST_DistanceSphere(geom, ST_MakePoint(:lon, :lat)) <= :distance" +
+                "ORDER BY ST_Distance (geom\\:\\:geography, ST_Point(:lon, :lat)\\:\\:geography) ASC LIMIT :max";
+            
+    @Query(value= query, nativeQuery = true)
     List<Dallas> findThreeNearest(@Param("long") Double lon, @Param("lat") Double lat, @Param("distance") Double distance, @Param("max") Double max);
 }
